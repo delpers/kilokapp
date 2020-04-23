e9d46aa2f4dc3a42e3138055a85914be091ae057
delpers <c.boschat@protonmail.com>
2020-04-23 15:34:19 +0200

    Design

--- src/templates/e-recipe-page.js
+++ src/templates/e-recipe-page.js
@@ -70,85 +70,65 @@ const cookingRecipe = props => {
 <div className="mb-32 sticky b-solid-b">
 
 
-  <div class="m-w p-i pb-0">
-    <span class="i-link fs-16 b-b-g mr-15 font-bold">{props.data.CRE.ingredientsNumbers} Ing(s).</span>
-    <span class="i-link fs-16 b-b-g mr-15 font-bold">{props.data.CRE.numberOfPersons}</span>
+  <div class="m-w p-i ">
+    <span class="i-link fs-16 b-b-g mr-15 font-bold text-gray">{props.data.CRE.numberOfPersons}</span>
 
-    <div class="t-d fl-r mb-15"><span class="fs-14 bg-g">
+    <div class="t-d fl-r mb-15 mt_i5"><span class="fs-14 bg-g">
       <i class="fas fa-check-circle"></i> {props.data.CRE.time} </span></div>
 
     
 
     </div>
     </div>
+    <h3 class="i-link b-b-g mr-15 font-bold m-w p-i bg-w">{props.data.CRE.ingredientsNumbers} Ingrédient(s).</h3>
 
-        <div className="ingredientsList m-w p-i">
+        <div className="sr-cards m-w p-i mt-1 bg-w">
           {props.data.CRE.nIngredients.map(dataIGRD => {
             return (
-              <div id={dataIGRD.id} className="rounded mb-32">
-                <div>
-                  <picture>
-                    <img
-                      src={dataIGRD.iMg.secure_url}
-                      alt={dataIGRD.title}
-                    ></img>
-                  </picture>
-                  <h3>{dataIGRD.title}</h3>
-                  <span>{dataIGRD.amount}</span>
+              <div id={dataIGRD.id} className="rounded mb-32 mb-20 bg-w rounded shadow-sm bg-w-c">
+                <div  className="ingr_mediap" style={{
+                backgroundImage:
+                  "url(" +
+                  dataIGRD.iMg.secure_url +
+                  ")",
+                backgroundPosition: "center",
+                backgroundSize: "contain",
+                backgroundRepeat: "no-repeat",
+                height: "160px",
+                width: "160px",
+                margin: "20px",
+              }}>
+            
                 </div>
+                     <div className="mt-10 p-15 fs-16 _print_w">
+                <h3 className="i-link fs-16 b-b-g mr-15 font-bold mb-0 nowrap">{dataIGRD.title}</h3>
+                  <span class="fs-16 text-gray pr-15">{dataIGRD.amount}</span>
               </div>
+              </div>
+
             )
           })}
         </div>
 
-      <div className="m-w p-i">
+      <div className="m-w p-i bg-w">
         <div>
           <div>
             {props.data.CRE.instructions.map(dataIST => (
-              <div className="rounded mb-32" key={dataIST.instructions}>
+              <div className="rounded mb-32 mb-32 sticky b-solid-b direction" key={dataIST.instructions}>
                 {dataIST}
               </div>
             ))}
           </div>
 
-          <div className="end">
-            <div>
-              <label className="gomodal" for="showblock">
-                <i class="far fa-image"></i>
-              </label>
-              <input type="checkbox" id="showblock" />
-              <div id="modal">
-                <div className="container">
-                  <div className="modal-inner">
-                    <label className="close" for="showblock">
-                      
-                    </label>
-                    <div className="modal-side">
-                      <div
-                        style={{
-                          backgroundImage:
-                            "url(" +
-                            props.data.CRE
-                              .childContentfulCookingRecipeFeaturedImageJsonNode
-                              .secure_url +
-                            ")",
-                        }}
-                      />{" "}
-                    </div>
-                    <div></div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
       </div>
 
-      <div className="m-w p-i">
+      <div className="m-w p-i boosters">
         <div className="getBoosters getBoosters-mobile m-w p-i">
           {props.data.CRE.boosters.map(dataFLC => {
             return (
-              <div>
+              <div className="mb-20 bg-w rounded shadow-sm">
                 <div>
                   <picture>
                     <img
