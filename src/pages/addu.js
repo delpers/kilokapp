48d9787dcda1938359906676a10d2cc006b97e3e
delpers <63206315+delpers@users.noreply.github.com>
2020-06-30 13:03:59 +0200

    Design

--- src/pages/addu.js
+++ src/pages/addu.js
@@ -240,11 +240,12 @@ const RegisterPage = () => {
                <div className="userplan">
                  <div className="badgeblue">1 mois gratuits</div>
                  <h3 class="plan_name">Standard</h3>
-                  <p className="detailsuserplan">9,99 €/mois après la période de l'offre.</p>
+                  <p className="detailsuserplan">14,99 €/mois après la période de l'offre.</p>
                   <div className="tb">
                     
                     <div className="fs-16"><i class="fas fa-check"></i> Blog</div>
                     <div className="fs-16"><i class="fas fa-check"></i> Recherche</div>
+                    <div className="fs-16"><i class="fas fa-check"></i> Entrainements & Exercises</div>
                     <div className="fs-16"><i class="fas fa-check"></i> Boosters & Breakfast</div>
                     <div className="fs-16"><i class="fas fa-check"></i> Méditation+</div>
 
@@ -262,19 +263,7 @@ const RegisterPage = () => {
 
 
                   <div className="userplan">
-                 <div className="badgeblue">1 mois gratuits</div>
-                 <h3 class="plan_name">Standard +</h3>
-                  <p className="detailsuserplan">14,99 €/mois après la période de l'offre.</p>
-                  <div className="tb">
-                    
-                    <div className="fs-16"><i class="fas fa-check"></i> Blog</div>
-                    <div className="fs-16"><i class="fas fa-check"></i> Recherche</div>
-                    <div className="fs-16"><i class="fas fa-check"></i> Entrainements & Exercises</div>
-                    <div className="fs-16"><i class="fas fa-check"></i> Boosters & Breakfast</div>
-                    <div className="fs-16"><i class="fas fa-check"></i> Méditation+</div>
-
-
-                  </div>
+                 - -
                   </div>
                  
 
